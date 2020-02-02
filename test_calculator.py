673e7ac7fd65b76fa5ba25028fd06b09ad1e79a6
namounyoucef <namoun.youcef.1@gmail.com>
2020-02-02 15:47:49 +0100

    python

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+import unittest
+from app.calculator import Calculator
+class TddInPythonExample(unittest.TestCase):
+    def setUp(self):
+       self.calc = Calculator()
+    def test_calculator_add_method_returns_correct_result(self):
+        calc = Calculator()
+        result = calc.add(2,2)
+        self.assertEqual(4, result)
+    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+        self.assertRaises(ValueError, self.calc.add, 'un', 'deux')
+
+if __name__ == '__main__':
+    unittest.main()
